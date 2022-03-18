9e24016ff1ff110ad6240f98dd7b066a3796d01a
Lin Yinfeng <lin.yinfeng@outlook.com>
2022-03-18 15:46:27 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -5,10 +5,7 @@ let
   forceDrv = file: pkgs.runCommand "spec.json" {} ''
     install --mode=444 "${file}" $out
   '';
-  projects = {
-    "dotfiles" = forceDrv ./projects/dotfiles.json;
-  };
-  jobsets = projects.${projectName};
+  jobsets = forceDrv ./projects/${projectName}.json;
 in
 {
   inherit jobsets;
